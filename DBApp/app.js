7cac855ef60e73484fcd33603e273163b68e4130
Brandon Hofbauer <71418632+brandonhofbauer@users.noreply.github.com>
2023-03-09 15:29:55 -0800

    Update app.js
    Updated app.js for Brandon's pages

--- DBApp/app.js
+++ DBApp/app.js
@@ -8,7 +8,7 @@ var app     = express();            // We need to instantiate an express object 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))   // Enable express to handle json and form data
-PORT        = 9001;                 // Set a port number at the top so it's easy to change in the future
+PORT        = 9002;                 // Set a port number at the top so it's easy to change in the future
 
 const { engine } = require('express-handlebars');
 var exphbs = require('express-handlebars');     // Import express-handlebars
@@ -35,13 +35,20 @@ app.get('/employees', function(req, res){
     res.render('employees')
     });
 
-app.get('/items_ordered', function(req, res){
-    res.render('items_ordered')
+app.get('/items_ordered', function(req, res)
+    {
+        let query1 = "SELECT * FROM Items_Ordered;";
+        db.pool.query(query1, function(error, rows, fields){
+            res.render('items_ordered', {data: rows});
+        })
     });
 
 app.get('/menuitems', function(req, res)
     {
-        res.render('menuitems')
+        let query1 = "SELECT * FROM MenuItems;";
+        db.pool.query(query1, function(error, rows, fields){
+            res.render('menuitems', {data: rows});
+        })
     });
 
 app.get('/order_employees', function(req, res){
@@ -52,14 +59,22 @@ app.get('/orders', function(req, res){
     res.render('orders')
     });
 
-app.get('/recipes', function(req, res){
-    res.render('recipes')
+app.get('/recipes', function(req, res)
+    {
+        let query1 = "SELECT * FROM Recipes;";
+        db.pool.query(query1, function(error, rows, fields){
+            res.render('recipes', {data: rows});
+        })
     });
 
-app.get('/inventoryitems', function(req, res){
-    res.render('inventoryitems')
+app.get('/inventoryitems', function(req, res)
+    {
+        let query1 = "SELECT * FROM InventoryItems;";
+        db.pool.query(query1, function(error, rows, fields){
+            res.render('inventoryitems', {data: rows});
+        })
     });
-
+ 
 app.post('/add-customer-ajax', function(req, res) 
 {
     // Capture the incoming data and parse it back to a JS object
@@ -106,11 +121,103 @@ app.post('/add-customer-ajax', function(req, res)
     })
 });
 
-app.delete('/delete-customer-ajax/', function(req,res,next){
+app.post('/add-menu-ajax', function(req, res) 
+{
+    let data = req.body;
+
+    query1 = `INSERT INTO MenuItems (item, type, price) VALUES ('${data.item}', '${data.type}', '${data.price}')`;
+    db.pool.query(query1, function(error, rows, fields){
+        if (error) { 
+            console.log(error)
+            res.sendStatus(400);
+        } else {
+            query2 = `SELECT * FROM MenuItems;`;
+            db.pool.query(query2, function(error, rows, fields){
+                if (error) {
+                    console.log(error);
+                    res.sendStatus(400);
+                } else {
+                    res.send(rows);
+                }
+            })
+        }
+    })
+});
+
+app.post('/add-inventory-ajax', function(req, res) 
+{
+    let data = req.body;
+
+    query1 = `INSERT INTO InventoryItems (item, type, quantity) VALUES ('${data.item}', '${data.type}', '${data.quantity}')`;
+    db.pool.query(query1, function(error, rows, fields){
+        if (error) { 
+            console.log(error)
+            res.sendStatus(400);
+        } else {
+            query2 = `SELECT * FROM InventoryItems;`;
+            db.pool.query(query2, function(error, rows, fields){
+                if (error) {
+                    console.log(error);
+                    res.sendStatus(400);
+                } else {
+                    res.send(rows);
+                }
+            })
+        }
+    })
+});
+
+app.post('/add-recipe-ajax', function(req, res) 
+{
+    let data = req.body;
+
+    query1 = `INSERT INTO Recipes (menuID, inventoryID, reqAmt) VALUES ('${data.menu}', '${data.inventory}', '${data.reqAmt}')`;
+    db.pool.query(query1, function(error, rows, fields){
+        if (error) { 
+            console.log(error)
+            res.sendStatus(400);
+        } else {
+            query2 = `SELECT * FROM Recipes;`;
+            db.pool.query(query2, function(error, rows, fields){
+                if (error) {
+                    console.log(error);
+                    res.sendStatus(400);
+                } else {
+                    res.send(rows);
+                }
+            })
+        }
+    })
+});
+
+app.post('/add-itemsordered-ajax', function(req, res) 
+{
+    let data = req.body;
+
+    query1 = `INSERT INTO Items_Ordered (orderID, menuID) VALUES ('${data.order}', '${data.menu}')`;
+    db.pool.query(query1, function(error, rows, fields){
+        if (error) { 
+            console.log(error)
+            res.sendStatus(400);
+        } else {
+            query2 = `SELECT * FROM Items_Ordered;`;
+            db.pool.query(query2, function(error, rows, fields){
+                if (error) {
+                    console.log(error);
+                    res.sendStatus(400);
+                } else {
+                    res.send(rows);
+                }
+            })
+        }
+    })
+});
+
+app.delete('/delete-customer-ajax', function(req,res,next){
     let data = req.body;
     let customerID = parseInt(data.id);
     let updateOrders = `UPDATE Orders SET customerID = NULL WHERE customerID = ?`;
-    let deleteCustomer= `DELETE FROM Customers WHERE customerID = ?`;
+    let deleteCustomer = `DELETE FROM Customers WHERE customerID = ?`;
   
   
           // Run the 1st query
@@ -137,43 +244,188 @@ app.delete('/delete-customer-ajax/', function(req,res,next){
               }
   })});
 
-  app.put('/put-customer-ajax', function(req,res,next){
+app.delete('/delete-menu-ajax', function(req,res,next){
     let data = req.body;
+    let menuID = parseInt(data.id);
+    let deleteItems_Ordered = `DELETE FROM Items_Ordered WHERE menuID = ?`;
+    let deleteMenu = `DELETE FROM MenuItems WHERE menuID = ?`;
 
-    let customerID = data.customer;
-    let name = data.name;
-    let phone = data.phone;
-    let email = data.email;
-    let address = data.address;
-  
-    let updateCustomer = `UPDATE Customers SET name = ?, phone = ?, email = ?, address = ? WHERE Customers.customerID = ?`;
-    let selectCustomer = `SELECT * FROM Customers WHERE customerID = ?`
+        db.pool.query(deleteItems_Ordered, [menuID], function(error, rows, fields){
+            if (error) {
 
-          // Run the 1st query
-          db.pool.query(updateCustomer, [name, phone, email, address, customerID], function(error, rows, fields){
-              if (error) {
-  
-              // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
-              console.log(error);
-              res.sendStatus(400);
-              }
-  
-              // If there was no error, we run our second query and return that data so we can use it to update the people's
-              // table on the front-end
-              else
-              {
-                  // Run the second query
-                  db.pool.query(selectCustomer, [customerID], function(error, rows, fields) {
-  
-                      if (error) {
-                          console.log(error);
-                          res.sendStatus(400);
-                      } else {
-                          res.send(rows);
-                      }
-                  })
-              }
-  })});
+            console.log(error);
+            res.sendStatus(400);
+            } else {
+                db.pool.query(deleteMenu, [menuID], function(error, rows, fields) {
+                    if (error) {
+                        console.log(error);
+                        res.sendStatus(400);
+                    } else {
+                        res.sendStatus(204);
+                    }
+                })
+            }
+})});
+
+app.delete('/delete-inventory-ajax', function(req,res,next){
+    let data = req.body;
+    let inventoryID = parseInt(data.id);
+    let deleteInventory = `DELETE FROM InventoryItems WHERE inventoryID = ?`;
+
+        db.pool.query(deleteInventory, [inventoryID], function(error, rows, fields){
+            if (error) {
+                console.log(error);
+                res.sendStatus(400);
+            } else {
+                res.sendStatus(204);
+            }
+})});
+
+app.delete('/delete-recipe-ajax', function(req,res,next){
+    let data = req.body;
+    let recipeID = parseInt(data.id);
+    let deleteRecipe = `DELETE FROM Recipes WHERE recipeID = ?`;
+
+        db.pool.query(deleteRecipe, [recipeID], function(error, rows, fields){
+            if (error) {
+                console.log(error);
+                res.sendStatus(400);
+            } else {
+                res.sendStatus(204);
+            }
+})});
+
+app.delete('/delete-itemsordered-ajax', function(req,res,next){
+    let data = req.body;
+    let itemsorderedID = parseInt(data.id);
+    let deleteItemsOrdered = `DELETE FROM Items_Ordered WHERE itemsorderedID = ?`;
+
+        db.pool.query(deleteItemsOrdered, [itemsorderedID], function(error, rows, fields){
+            if (error) {
+                console.log(error);
+                res.sendStatus(400);
+            } else {
+                res.sendStatus(204);
+            }
+})});
+
+app.put('/put-customer-ajax', function(req,res,next){
+let data = req.body;
+
+let customerID = data.customer;
+let name = data.name;
+let phone = data.phone;
+let email = data.email;
+let address = data.address;
+
+let updateCustomer = `UPDATE Customers SET name = ?, phone = ?, email = ?, address = ? WHERE Customers.customerID = ?`;
+let selectCustomer = `SELECT * FROM Customers WHERE customerID = ?`
+
+        // Run the 1st query
+        db.pool.query(updateCustomer, [name, phone, email, address, customerID], function(error, rows, fields){
+            if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error);
+            res.sendStatus(400);
+            }
+
+            // If there was no error, we run our second query and return that data so we can use it to update the people's
+            // table on the front-end
+            else
+            {
+                // Run the second query
+                db.pool.query(selectCustomer, [customerID], function(error, rows, fields) {
+
+                    if (error) {
+                        console.log(error);
+                        res.sendStatus(400);
+                    } else {
+                        res.send(rows);
+                    }
+                })
+            }
+})});
+
+app.put('/put-menu-ajax', function(req,res,next){
+    let data = req.body;
+    
+    let menuID = data.menu;
+    let item = data.item;
+    let type = data.type;
+    let price = data.price;
+    
+    let updateMenu = `UPDATE MenuItems SET item = ?, type = ?, price = ? WHERE MenuItems.menuID = ?`;
+    let selectMenu = `SELECT * FROM MenuItems WHERE menuID = ?`
+    
+            db.pool.query(updateMenu, [item, type, price, menuID], function(error, rows, fields){
+                if (error) {
+    
+                console.log(error);
+                res.sendStatus(400);
+                } else {
+                    db.pool.query(selectMenu, [menuID], function(error, rows, fields) {
+                        if (error) {
+                            console.log(error);
+                            res.sendStatus(400);
+                        } else {
+                            res.send(rows);
+                        }
+                    })
+                }
+})});
+
+app.put('/put-inventory-ajax', function(req,res,next){
+    let data = req.body;
+    
+    let inventoryID = data.inventory;
+    let item = data.item;
+    let type = data.type;
+    let quantity = data.quantity;
+    
+    let updateInventory = `UPDATE InventoryItems SET item = ?, type = ?, quantity = ? WHERE InventoryItems.inventoryID = ?`;
+    let selectInventory = `SELECT * FROM InventoryItems WHERE inventoryID = ?`
+            db.pool.query(updateInventory, [item, type, quantity, inventoryID], function(error, rows, fields){
+                if (error) {
+                    console.log(error);
+                    res.sendStatus(400);
+                } else {
+                    db.pool.query(selectInventory, [inventoryID], function(error, rows, fields) {
+                        if (error) {
+                            console.log(error);
+                            res.sendStatus(400);
+                        } else {
+                            res.send(rows);
+                        }
+                    })
+                }
+})});
+
+app.put('/put-recipe-ajax', function(req,res,next){
+    let data = req.body;
+    
+    let recipeID = data.recipe;
+    let menu = data.menu;
+    let inventory = data.inventory;
+    let reqAmt = data.reqAmt;
+    
+    let updateRecipe = `UPDATE Recipes SET menuID = ?, inventoryID = ?, reqAmt = ? WHERE Recipes.recipeID = ?`;
+    let selectRecipe = `SELECT * FROM Recipes WHERE recipeID = ?`
+            db.pool.query(updateRecipe, [menu, inventory, reqAmt, recipeID], function(error, rows, fields){
+                if (error) {
+                    console.log(error);
+                    res.sendStatus(400);
+                } else {
+                    db.pool.query(selectRecipe, [recipeID], function(error, rows, fields) {
+                        if (error) {
+                            console.log(error);
+                            res.sendStatus(400);
+                        } else {
+                            res.send(rows);
+                        }
+                    })
+                }
+})});
 
 /*
     LISTENER
